eaf3a9be2e24a53fbd3dc351431a2e0008487ff1
Andrew McIndoe <amcindoe@powerhrg.com>
2021-10-09 12:54:35 -0400

    commit tree c72d7fd9294d5eedb0ac418b2c941c7f42a7d5fd

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  resources :sessions
+
+    get "login", to: "sessions#new"
+    post "login", to: "sessions#create"
+    get "logout", to: "sessions#destroy"
+    post "logout", to: "sessions#destroy"
+    root to: "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
